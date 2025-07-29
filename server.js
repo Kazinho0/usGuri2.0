68cbd91620222325ba4c5030acbf11b988537704
Kazinho <otakinhogtg@gmail.com>
2025-07-29 00:02:12 -0300

    ve ae

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const PORT = 3000;
 // Servir arquivos estáticos da pasta atual (usGuri2.0)
 app.use(express.static(__dirname));
 
-// Rota /login para servir o main.html
-app.get('/login', (req, res) => {
+// Rota / para servir o main.html
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'main.html'));
 });
 
@@ -20,3 +20,9 @@ app.get('/home', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+const uri = 'mongodb+srv://Kazinho:kazinho3344@cluster0.xyftxx8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(uri)
+    .then(() => console.log('Conectado ao MongoDB'))
+    .catch(err => console.error('Erro ao conectar ao MongoDB', err));
